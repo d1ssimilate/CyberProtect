b332c43fab2bf7ce54d004b8197a3e194f7d1ba3
d1ssimilate <makarovroman6284@gmail.com>
2024-08-20 19:13:30 +0400

    recommendation output media

--- src/components/Dialog/Recommendation/Recommendation.tsx
+++ src/components/Dialog/Recommendation/Recommendation.tsx
@@ -2,10 +2,38 @@ import { useContext } from "react";
 import styles from "./Recommendations.module.scss";
 import { DialogContext } from "../../Providers/DialogProvier/DialogProvider";
 import { TRecommendationRequestData } from "../../../api/entities/recommendation/recommendation.types";
+import { url } from "../../../api/instance";
+import { Image } from "primereact/image";
 
 export function RecommendationDialog() {
   const { data: ContextData } = useContext(DialogContext);
   const data = ContextData as TRecommendationRequestData;
 
-  return <p className={styles.description}>{data.description}</p>;
+  return (
+    <div className={styles.content}>
+      <p className={styles.description}>{data.description}</p>
+      {data.attachments &&
+        data.attachments.map((item) => {
+          const fileUrl = `${url}${item.url}`;
+          return (
+            <>
+              {item.type === "image" ? (
+                <Image
+                  className={styles.img}
+                  preview={true}
+                  alt={item.label}
+                  src={fileUrl}
+                />
+              ) : item.type === "video" ? (
+                <video src={fileUrl} controls />
+              ) : (
+                <span style={{ color: "var(--deep-blue)", fontSize: "18px" }}>
+                  {item.label}
+                </span>
+              )}
+            </>
+          );
+        })}
+    </div>
+  );
 }
