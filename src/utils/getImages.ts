76b62a0568ecb9e4905e6eb48d327364d7dabdc2
VladISLAV <vlad19022005@mail.ru>
2024-08-24 01:11:28 +0400

    add: more icon

--- src/utils/getImages.ts
+++ src/utils/getImages.ts
@@ -6,5 +6,11 @@ export function getImages() {
     "/christmas-toys/gift.svg",
     "/christmas-toys/snowman.svg",
     "/christmas-toys/snowGlobe.svg",
+    "/christmas-toys/christmas-bag_3778399.svg",
+    "/christmas-toys/santa-claus_3723065(1).svg",
+    "/christmas-toys/gift_16832028.svg",
+    "/christmas-toys/sparkler_3778434.svg",
+    "/christmas-toys/christmas-wreath_3778515.svg",
+    "/christmas-toys/santa-hat_3778848(1).svg",
   ];
 }
